dadc14049c5df4b1883ba2ba5c2d391c1722b793
Andrew Koerner <andrewkoerner.b@gmail.com>
2011-02-24 08:45:49 -0600

    refining framework

--- reports.rb
+++ reports.rb
@@ -2,18 +2,23 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
-configure do 
-	set :root, File.dirname(__FILE__)
+class Reports < Sinatra::Base
+
+ configure do
+    set :root,    File.dirname(__FILE__)
+    set :views,   File.join(Sinatra::Application.root, 'views')
+    set :haml,    {:format => :html5, :layout => :reports }
+  end
+  
+  get '/?' do
+    haml :index
+  end
+
+  not_found do
+    redirect '/'
+  end
+  
+  get '/:input' do
+    params[:input]
+  end
 end
-
-
-get '/?' do
-  "Hello World!"
-end
-
-
-get '/:input' do
-  params[:input]
-
-end
-
