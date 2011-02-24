06ad1bb29d9987fa5f92594d77fd99159b2cb2b2
Andrew Koerner <andrewkoerner.b@gmail.com>
2011-02-24 11:21:20 -0600

    fixed some issues with the includes

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "http://rubygems.org"
 gem 'sinatra'
+gme 'compass'
 gem 'haml'
 gem 'dm-migrations'
 
--- reports.rb
+++ reports.rb
@@ -2,15 +2,16 @@ require 'rubygems'
 require 'dm-core'
 require 'sinatra'
 require 'haml'
-require  'dm-migrations'
-Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
+require 'dm-migrations'
+#require 'lib/entries'
+#require 'lib/goods'
+#require 'lib/products'
 
 class Reports < Sinatra::Base
 
  configure do
     set :root,    File.dirname(__FILE__)
     set :views,   File.join(Sinatra::Application.root, 'views')
-    set :haml,    {:format => :html5, :layout => :reports }
 	
 	#datamapper init
     DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/reports.db")
@@ -19,7 +20,7 @@ class Reports < Sinatra::Base
   end
   
   get '/?' do
-	haml :show, :layout => :reports 
+    "Hello World"
   end
 
   not_found do
