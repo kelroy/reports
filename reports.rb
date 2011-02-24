a97d3322a61d0fafa5f56a70ac9251dafcd36f0b
Andrew Koerner <andrewkoerner.b@gmail.com>
2011-02-24 10:22:55 -0600

    added datamapper init and db to reports.rb

--- reports.rb
+++ reports.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
+require 'dm-core'
 require 'sinatra'
 require 'haml'
+require  'dm-migrations'
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 
 class Reports < Sinatra::Base
 
@@ -8,6 +11,11 @@ class Reports < Sinatra::Base
     set :root,    File.dirname(__FILE__)
     set :views,   File.join(Sinatra::Application.root, 'views')
     set :haml,    {:format => :html5, :layout => :reports }
+	
+	#datamapper init
+    DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/reports.db")
+    DataMapper.finalize
+    DataMapper.auto_migrate!
   end
   
   get '/?' do
